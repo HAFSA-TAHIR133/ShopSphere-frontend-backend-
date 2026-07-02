7b4dc1429077e37c6c675d8be1d4b87c3217b264
HAFSA-TAHIR133 <hn486916@gmail.com>
2026-07-02 16:37:04 +0500

    Delete ShopSphere Backend+Frontend Project directory

--- ShopSphere Backend+Frontend Project/Backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import AuthController from '../controllers/authControllers.js';
-import express from 'express';
-const router=express.Router();
-
-router.post("/signup", AuthController.signup);
-router.post("/login", AuthController.login);
-router.post("/logout", AuthController.logout);
-
-export default router;
