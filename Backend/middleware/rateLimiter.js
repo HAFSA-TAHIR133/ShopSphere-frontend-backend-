5cc933c0bee8382836f0d2e9d0afc278037762bf
HAFSA-TAHIR133 <hn486916@gmail.com>
2026-07-03 16:54:49 +0500

    Delete Backend directory

--- Backend/middleware/rateLimiter.js
+++
@@ -1,8 +0,0 @@
-import rateLimit from "express-rate-limit";
-export const limiter =rateLimit({
-  windowMs:5*60*1000,
-  limit:100,
-  message:"Too many request."
-});
-
-// export default limiter;
--- Backend/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import ProductController from '../controllers/productControllers.js';
-
-const router = express.Router();
-
-router.get('/products', ProductController.getProducts);
-router.get('/products/:id', ProductController.getProductById);
-export default router;
